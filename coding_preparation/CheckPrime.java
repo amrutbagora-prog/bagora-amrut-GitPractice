de690d27e46dcae5eda79c467cc0a68a92f93bfe
amrutbagora-prog <amrutbagora@gmail.com>
2026-07-25 19:23:49 +0530

    Revert "un-optimised code for checking prime"
    This reverts commit 2dcadcc436a5976b04a241a7276165ffcca6e792.

---
+++ coding_preparation/CheckPrime.java
@@ -0,0 +1,28 @@
+
+public class CheckPrime {
+
+    // Optimized prime-checking method using square-root logic
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        
+        // Loop only up to the square root of n
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int number = 29; 
+        if (isPrime(number)) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+    }
+}
+ 
